e9fadf0c9baae6f6a81f95b4448adc3c357040cf
vinamarcyella0410 <ipincantik04@gmail.com>
2020-07-11 06:32:36 +0800

    First commit

---
+++ whitelistku/lib/values/bahasa.dart
@@ -0,0 +1,37 @@
+class Bahasa {
+  static final String tagPrimaryButton = "primarybutton";
+  static final String simpan = "Simpan Sekarang";
+  static final String masihKosong = "Yahh Masih Kosong :( ";
+  static final String harga = "Harga";
+  static final String hargaTidakValid = "Harga tidak valid";
+  static final String deskripsi = "Deskripsi";
+  static final String namaItem = "Nama Barang";
+  static final String kategori = "Kategori";
+  static final String tercapai = "Tercapai";
+  static final String targetTercapaiTitle = "Yeay Target Tercapai";
+  static final String targetTercapaiDetail =
+      "Apakah targetmu sudah benar tercapai? jika sudah selamat ya.Yuk Lanjutkan >.<";
+  static final String hapus = "Hapus Aja";
+  static final String tutup = "Tutup Dulu";
+  static final String konfirmasiHapusTitle = "Hapus Berhasil";
+  static final String konfirmasiHapusDetail =
+      "Setelah dihapus, sayangnya item tidak bisa dikembalikan :( Kamu Yakin ingin menghapusnya?";
+  static final String batalkan = "Batalkan Saja";
+  static final String lanjutkan = "Tercapai";
+  static final String mengerti = "Mengerti";
+
+  static final String pesanWhitelistKosong = "Yuk list belanjaanmu bisini Dan dapatkan segala kemudahan dalam mengingat semua kebutuhan harianmu.Happy Shopping ^^";
+  static final String pesanKategoriKosong = "Yuk beri kategori sesuai dengan belanjaanmu";
+  static final String pilihIconTitle = "Ayo Pilih Ikon Dulu Ya>.<";
+  static final String pilihIconDetail =
+      "Upss...Untuk melanjutkan, kamu harus memilih ikon terlebih dahulu";
+  static final String kategoriKosongMsgDetail =
+      "Huff...Sepertinya kamu belum memiliki daftar kategori,Tambahkan dulu ya >.<";
+  static final String ops = "Ops!";
+
+  static final String tambahWhitelist = "Tambah List Belanja";
+  static final String tambahKategori = "Tambah Kategori";
+  static final String pilihIkon = "Pilih Ikon";
+  static final String whitelistKu = "List Belanja Seru";
+  static final String daftarKategori = "Daftar Kategori";
+}
